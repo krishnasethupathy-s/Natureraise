e2bb12daa7fda23001873a52e0ba141a0d7cc5cf
Karthik A <karthikthee7@gmail.com>
2022-06-17 19:15:43 +0530

    add order detail

--- src/components/Natureraise/Common/Components/OrderDetailsCard/OrderDetailsCard.js
+++ src/components/Natureraise/Common/Components/OrderDetailsCard/OrderDetailsCard.js
@@ -1,41 +1,43 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import "./OrderDetailsCard.css";
-import {Col,Row} from 'react-bootstrap';
+import { Col, Row } from "react-bootstrap";
 
 const OrderDetailsCard = (props) => {
   return (
     <div className="order_card_wrap">
-      <Row>
-        <Col md={6}>
-          <div className="order_card_img">
-            <img
-              src="https://www.solarclue.com/image/cache/catalog/Products/Solar%20Water%20Heater/V-Guard/v-guard-solar-water-heater-fpc-systems-nw-500x500-600x600-222x222.png"
-              className="img-fluid"
-              alt="Best Ecommerce natureraise"
-            />
-            <div className="order_card_title">
-              <h6 className="order_card_heading">
-               {props.heading}
-              </h6>
-              <h6 className="order_card_sub">{props.color}</h6>
-              <h6 className="order_card_sub">{props.subtitle}</h6>
+      <Link to={`/orderDetails/${props.order_id}`}>
+        <Row>
+          <Col md={6}>
+            <div className="order_card_img">
+              <img
+                src="https://www.solarclue.com/image/cache/catalog/Products/Solar%20Water%20Heater/V-Guard/v-guard-solar-water-heater-fpc-systems-nw-500x500-600x600-222x222.png"
+                className="img-fluid"
+                alt="Best Ecommerce natureraise"
+              />
+              <div className="order_card_title">
+                <h6 className="order_card_heading">{props.heading}</h6>
+                <h6 className="order_card_sub">{props.color}</h6>
+                <h6 className="order_card_sub">{props.subtitle}</h6>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col md={2} className="order_card_amount_wrap">
-          <div className="order_card_amount">
-            <h6 className="order_card_sub">${props.amount}</h6>
-          </div>
-        </Col>
-        <Col md={4}>
-          <div className="order_card_review_wrap">
-            <div>
-              <h6 className="order_review_title">{props.deliverydate}</h6>
-              <h6 className="order_review_sub">{props.deleiverystatus}</h6>
+          </Col>
+          <Col md={2} className="order_card_amount_wrap">
+            <div className="order_card_amount">
+              <h6 className="order_card_sub">${props.amount}</h6>
             </div>
-          </div>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={4}>
+            <div className="order_card_review_wrap">
+              <div>
+                <h6 className="order_review_title">{props.deliverydate}</h6>
+                <h6 className="order_review_sub">{props.deleiverystatus}</h6>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Link>
     </div>
   );
 };
--- src/components/Natureraise/OrderStatus/constant/AddressDetails/AddressDetails.js
+++ src/components/Natureraise/OrderStatus/constant/AddressDetails/AddressDetails.js
@@ -1,29 +1,49 @@
 import React from "react";
 import "./AddressDetails.css";
-import {Row,Col} from 'react-bootstrap';
+import { Row, Col } from "react-bootstrap";
 
-const AddressDetails = (props) => {
+const AddressDetails = ({ detail }) => {
   return (
     <Row>
       <Col md={4}>
         <div className="order_address_wrap">
-          <h6 className="order_title">Deleivery Address</h6>
-          <h6 className="order_name">Karthik</h6>
+          <h6 className="order_title">Delivery Address</h6>
+          <h6 className="order_name">{detail.delivery_contact_name}</h6>
           <h6 className="order_address">
-            {" "}
-            No: 111, kk, Nagar, Mangadu,Chennai
+            {`${detail.delivery_address_line1}, ${detail.delivery_address_line2} `}
           </h6>
-          <h6 className="order_address">Mobile Number: +91 7092933603</h6>
+          <h6 className="order_address">
+            {`${detail.delivery_city}, ${detail.delivery_state} `}
+          </h6>
+          <h6 className="order_address">Pin: {detail.delivery_pincode}</h6>
+          <h6 className="order_address">
+            Land Mark: {detail.delivery_landmark}
+          </h6>
+          <h6 className="order_address">
+            Mobile Number: {detail.delivery_mobile_number}
+          </h6>
 
-          <h6 className="order_address"> This is also tracked by 7092933603</h6>
+          {/* <h6 className="order_address"> This is also tracked by 7092933603</h6> */}
         </div>
       </Col>
       <Col md={3}>
         <div className="order_address_wrap">
-          <h6 className="order_title">Your Rewards</h6>
-          <div className="order_image_wrap">
+          <h6 className="order_title">Order Details</h6>
+
+          <h6 className="order_address">Order ID: {detail.order_id}</h6>
+          <h6 className="order_address">Date: {detail.entry_date}</h6>
+          <h6 className="order_address">
+            Payment Type:{" "}
+            {detail.payment_type === "0" ? "Online" : "Pay on Delivery"}
+          </h6>
+          <h6 className="order_address">
+            Payment Status: {detail.payment_status}
+          </h6>
+
+          {/* <div className="order_image_wrap">
             <img
               className="order_image_size"
+              alt="power icon"
               src="https://image.shutterstock.com/image-vector/electric-power-vector-icon-260nw-1140395627.jpg"
             />
             <div className="order_subtitle_wrap">
@@ -32,21 +52,26 @@ const AddressDetails = (props) => {
                 will be Share return period is over
               </h6>
             </div>
-          </div>
+          </div> */}
         </div>
       </Col>
       <Col md={3}>
         <div className="order_address_wrap">
-          <h6 className="order_title">More Action</h6>
-          <div className="order_image_wrap">
+          <h6 className="order_title">Amount</h6>
+          {/* <div className="order_image_wrap">
             <img
               className="order_image_size"
+              alt="Power Icon"
               src="https://image.shutterstock.com/image-vector/electric-power-vector-icon-260nw-1140395627.jpg"
             />
             <div className="order_subtitle_wrap">
               <h6 className="order_subtitle">Invoice Download</h6>
             </div>
-          </div>
+          </div> */}
+          <h6 className="order_address">
+            Coupon Amount: ₹ {detail.coupon_amount}
+          </h6>
+          <h6 className="order_title">Total Amount: ₹ {detail.order_amount}</h6>
         </div>
       </Col>
       <Col md={2} className="order_download_wrap">
