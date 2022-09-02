6c57d7175d772e97e53b585afdde5cc20834fb1a
Karthi A <karthikthee7@gmail.com>
2022-09-02 14:44:06 +0530

    fix missing catch

--- src/components/Natureraise/store/actions/Sitedata/Banner.js
+++ src/components/Natureraise/store/actions/Sitedata/Banner.js
@@ -2,30 +2,32 @@ import Config from "../../../../../Config";
 
 export const BANNER_DATA = "BANNER_DATA";
 
-
-
 export const fetchBanners = () => {
   return async (dispatch) => {
-    const Authorization = Config.getRequestToken();
-    const banner_type = "DashBoard";
-    let response = await fetch(Config.BaseUrl + "GetBanners/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      },
-      body: JSON.stringify({ Authorization, banner_type }),
-    });
-    let responseJsonData = await response.json();
-    var str = responseJsonData[0]["banner_images"];
-    var temp = str.split(",");
-    var imagesArray = [];
-    for (var i = 0; i < temp.length; i++) {
-      if (temp[i] === "") {
-      } else {
-        imagesArray.push(temp[i]);
+    try {
+      const Authorization = Config.getRequestToken();
+      const banner_type = "DashBoard";
+      let response = await fetch(Config.BaseUrl + "GetBanners/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: JSON.stringify({ Authorization, banner_type }),
+      });
+      let responseJsonData = await response.json();
+      var str = responseJsonData[0]["banner_images"];
+      var temp = str.split(",");
+      var imagesArray = [];
+      for (var i = 0; i < temp.length; i++) {
+        if (temp[i] === "") {
+        } else {
+          imagesArray.push(temp[i]);
+        }
       }
+      dispatch({ type: "BANNER_DATA", banner_list: imagesArray });
+      return responseJsonData;
+    } catch (e) {
+      console.log(e);
     }
-    dispatch({ type: "BANNER_DATA", banner_list: imagesArray });
-    return responseJsonData;
   };
 };
