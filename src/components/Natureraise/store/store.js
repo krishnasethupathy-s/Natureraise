531add673f03eab39d34b4efb54a3879a32c35ba
Karthi A <karthikthee7@gmail.com>
2023-01-13 19:58:47 +0530

    fix redux production migration error

--- src/components/Natureraise/store/store.js
+++ src/components/Natureraise/store/store.js
@@ -8,6 +8,7 @@ import AddCustomerAddress from "./reducers/UserProfile/CustomerAddress";
 import ProductActions from "./reducers/Product/ProductActions";
 
 import { persistStore, persistReducer } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
 import OrderReducer from "./reducers/Order/OrderReducer";
 
@@ -23,6 +24,8 @@ const persistConfig = {
   key: "root",
   storage,
   blacklist: ["UserActions"],
+  version: 0,
+  stateReconciler: autoMergeLevel2,
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
