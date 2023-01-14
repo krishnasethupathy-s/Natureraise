abde01d7693a63589941a5488e69ebcfb5d54e04
Karthi A <karthikthee7@gmail.com>
2023-01-14 20:58:27 +0530

    fix mirgration problem

--- src/components/Natureraise/store/store.js
+++ src/components/Natureraise/store/store.js
@@ -7,7 +7,7 @@ import Banner from "./reducers/Sitedata/Banner";
 import AddCustomerAddress from "./reducers/UserProfile/CustomerAddress";
 import ProductActions from "./reducers/Product/ProductActions";
 
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, createMigrate } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
 import OrderReducer from "./reducers/Order/OrderReducer";
@@ -20,12 +20,27 @@ const rootReducer = redux.combineReducers({
   OrderReducer: OrderReducer,
 });
 
+const migrations = {
+  1: (state) => {
+    return {
+      ...state,
+      ProductActions: {
+        ...state.ProductActions,
+        coupons: [],
+        category_products: [],
+        category_data_list: [],
+      },
+    };
+  },
+};
+
 const persistConfig = {
   key: "root",
   storage,
   blacklist: ["UserActions"],
-  version: 0,
+  version: 2,
   stateReconciler: autoMergeLevel2,
+  migrate: createMigrate(migrations, { debug: false }),
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
